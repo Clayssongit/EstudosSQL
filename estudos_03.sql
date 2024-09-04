86ffcac6d3943e35dd4cdf0ad013f99b97228205
Claysson Eugenio <clayssongit@hotmail.com>
2024-09-04 11:54:44 -0300

    Update estudos_03.sql

--- estudos_03.sql
+++ estudos_03.sql
@@ -1,4 +1,4 @@
-USE estudos_sql;
+USE estudos_03;
 CREATE TABLE if not exists editora(
     cod_editora int PRIMARY KEY not null,
     nome varchar (40) not null,
@@ -7,7 +7,7 @@ CREATE TABLE if not exists editora(
 );
 INSERT INTO editora (cod_editora, nome, cidade, tel_contato)
     VALUES
-        (11005, 'Saraiva', 'São Paulo', 1130901010),
+        (11009, 'Saraiva', 'São Paulo', 1130901010),
         (61001, 'Eras', 'Brasília', 6134201818),
         (41001, 'Summer', 'Curitiba', 4132994040),
         (11004, 'Pontos', 'São Paulo', 1154201966),
@@ -19,10 +19,38 @@ INSERT INTO editora (cod_editora, nome, cidade, tel_contato)
         
 SELECT * FROM editora ORDER BY cod_editora;
 
+USE estudos_03;
 CREATE TABLE if not exists autor(
     cod_autor int PRIMARY KEY not null,
     nome varchar (40) not null,
-    nacionalidade varchar (20) not null,
-    dt_nascimento date
+    nascionalidade varchar (20) not null,
+    dt_nacimento date
 );
-INSERT INTO autor (nome, nascionalidade, )
+INSERT INTO autor (nome, nacionalidade, dt_nascimento, cod_autor )
+    VALUES  
+        ('Ethevaldo Siqueira', 'Brasileira', '1950-10-04', 1000),
+        ('Ana Lucia Jankovic Barduchi', 'Brasileira', '1991-03-31', 1001),
+        ('Adélia Prado', 'Brasileira', '1948-12-20', 1002),
+        ('Walter Isaacson', 'Americana', '2000-07-07', 1003),
+        ('Steven J. Scott', 'Americana', '2002-12-16', 1004),
+        ('C. J. Date', 'Americana', '1950-03-25', 1005),
+        ('Pedro Felipe', 'Brasileira', '1995-12-18', 1006),
+        ('Kim Shaolin', 'China', '1970-04-03', 1007);
+SELECT * FROM autor ORDER BY cod_autor;
+
+USE estudos_03;
+CREATE TABLE if not exists genero(
+    cod_genero int PRIMARY KEY not null,
+    genero varchar (20) not null,
+    dias_devolucao tinyint not null
+);
+INSERT INTO genero
+    VALUES  
+        (101, 'Infantil', 5),
+        (102,'Romance',30),
+        (103, 'Biografia', 40),
+        (104, 'Tecnologia', 15),
+        (105, 'Administração', 20),
+        (106, 'Poesia', 10);
+USE estudos_03;
+SELECT * FROM genero ORDER BY genero;
